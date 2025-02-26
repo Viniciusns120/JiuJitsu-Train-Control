d9edf6430f9bb29d81a984e5b005f8969131b37e
Vinicius Nascimento <viniciusns120@hotmail.com>
2025-02-26 11:25:16 -0300

    first commit

---
+++ treino/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+faixa_choices = (
+        ('B', 'BRANCA'),
+        ('A', 'AZUL'),
+        ('R', 'ROXA'),
+        ('M', 'MARROM'),
+        ('P', 'PRETA')
+    )
+
+class Alunos(models.Model):
+    
+
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    data_nascimento = models.DateField(null=True, blank=True)
+    faixa = models.CharField(max_length=1, choices=faixa_choices, default='B')
+
+    def __str__(self):
+        return self.nome
+
+
+class AulasConcluidas(models.Model):
+    aluno = models.ForeignKey(Alunos, on_delete=models.CASCADE)
+    data = models.DateField(auto_now_add=True)
+    faixa_atual = models.CharField(max_length=1, choices=faixa_choices, default='B')
+
+    def __str__(self):
+        return self.aluno.nome
